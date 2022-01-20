d16f1c09bcbfcf55e675c603b9f0041ceca29014
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-20 06:22:52 -0300

    populando carga de dadis

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,20 @@
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernando Candiotti', '123.456.789-10', 7500.00, TIMESTAMP WITH TIME ZONE '1993-01-13T07:44:12.12345Z', '28');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernando Candiotti', '398.366.840-26', 4500.00,
+TIMESTAMP WITH TIME ZONE '1993-01-13T18:42:12.12345Z','28');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João da Silva', '625.401.444-03', 2750.00,
+TIMESTAMP WITH TIME ZONE '1997-06-29T22:44:12.12345Z', '49');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Nélio Alves', '478.189.812-25', 50125.30,
+TIMESTAMP WITH TIME ZONE '1982-11-15T12:35:12.12345Z', '39');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Antonio Fagundes', '461.363.772-41', 2500.00,
+TIMESTAMP WITH TIME ZONE '2001-12-11T02:25:12.12345Z', '18');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Pedro Da Silva', '932.738.414-86', 1180.00,
+TIMESTAMP WITH TIME ZONE '2000-01-01T01:49:12.12345Z', '22');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marcos Santos', '041.768.755-93', 7600.00,
+TIMESTAMP WITH TIME ZONE '2005-07-24T06:52:12.12345Z', '16');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Jose Angelo', '843.717.975-04', 5670.00,
+TIMESTAMP WITH TIME ZONE '1963-04-04T06:52:12.12345Z', '59');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Silvia Adriana', '843.717.975-04', 1000.00,
+TIMESTAMP WITH TIME ZONE '1963-06-13T06:52:12.12345Z', '59');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Flavia Joana', '329.568.242-95', 4755.12,
+TIMESTAMP WITH TIME ZONE '1991-01-29T06:52:12.12345Z', '30');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Barbara Almeida', '245.442.173-93', 3547.22,
+TIMESTAMP WITH TIME ZONE '1991-03-26T06:52:12.12345Z', '30');
