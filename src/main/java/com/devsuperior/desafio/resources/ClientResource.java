2ac0ddfb5a5aed00e74408ba21d59c4dc918ad86
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-20 06:39:12 -0300

    find by id implementado

--- src/main/java/com/devsuperior/desafio/resources/ClientResource.java
+++ src/main/java/com/devsuperior/desafio/resources/ClientResource.java
@@ -1,16 +1,14 @@
 package com.devsuperior.desafio.resources;
 
 import com.devsuperior.desafio.dto.ClientDTO;
+import com.devsuperior.desafio.entities.Client;
 import com.devsuperior.desafio.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -30,6 +28,13 @@ public class ClientResource {
         Page<ClientDTO> list = service.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
 
 
 }
--- src/main/java/com/devsuperior/desafio/service/ClientService.java
+++ src/main/java/com/devsuperior/desafio/service/ClientService.java
@@ -3,10 +3,14 @@ package com.devsuperior.desafio.service;
 import com.devsuperior.desafio.dto.ClientDTO;
 import com.devsuperior.desafio.entities.Client;
 import com.devsuperior.desafio.repositories.ClientRepository;
+import com.devsuperior.desafio.service.exceptions.ResourceNotFoundExceptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -14,9 +18,18 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
+    @Transactional(readOnly = true)
     public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
-        Page<Client> list =repository.findAll(pageRequest);
+        Page<Client> list = repository.findAll(pageRequest);
         return list.map(ClientDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Optional<Client> cli = repository.findById(id);
+        Client entity = cli.orElseThrow(() -> new ResourceNotFoundExceptions("Cliente não encontrado"));
+        return new ClientDTO(entity);
+
     }
 
 }
