98da54132f138ef4075e9ffb5e6b6ac84b2d6d86
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-20 06:21:24 -0300

    populando carga de dadis

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernando Candiotti', '123.456.789-10', 7500.00, TIMESTAMP WITH TIME ZONE '1993-01-13T07:44:12.12345Z', '28');
