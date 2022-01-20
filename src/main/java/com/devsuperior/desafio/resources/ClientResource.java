75d15ed152a99581438eec79f42aa14e04f23b55
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-20 06:23:41 -0300

    busca paginada de clientes

--- src/main/java/com/devsuperior/desafio/resources/ClientResource.java
+++ src/main/java/com/devsuperior/desafio/resources/ClientResource.java
@@ -7,17 +7,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/client")
+@RequestMapping(value = "/clients")
 public class ClientResource {
 
     @Autowired
     ClientService service;
 
+    @GetMapping
     public ResponseEntity<Page<ClientDTO>> findAll(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
